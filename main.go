5cd22a60dcedd1301e076a05d7a3a8504d76ce6a
Jace UwU <104167836+JaceWoblee@users.noreply.github.com>
2023-02-02 15:36:06 +0100

    Clean Up

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", indexFunc)
-	r.Get("/pageOne", pOneFunc)
-	r.Get("/pageTwo", pTwoFunc)
-	r.Get("/pageThree", pThreeFunc)
 
 	r.Handle("/style/*", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
 	http.ListenAndServe(":8080", r)
@@ -27,39 +24,6 @@ func indexFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err = t.ExecuteTemplate(w, "index.html", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func pOneFunc(w http.ResponseWriter, r *http.Request) {
-	t, err := template.New("").ParseFiles("view\\pageOne.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = t.ExecuteTemplate(w, "pageOne.html", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func pTwoFunc(w http.ResponseWriter, r *http.Request) {
-	t, err := template.New("").ParseFiles("view\\pageTwo.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = t.ExecuteTemplate(w, "pageTwo.html", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func pThreeFunc(w http.ResponseWriter, r *http.Request) {
-	t, err := template.New("").ParseFiles("view\\pageThree.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = t.ExecuteTemplate(w, "pageThree.html", nil); err != nil {
 		log.Fatal(err)
 	}
 }
