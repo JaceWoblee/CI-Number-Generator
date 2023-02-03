3e6f5dbeedcb6fe0527391aaac42ff9ee4b00372
Jace UwU <104167836+JaceWoblee@users.noreply.github.com>
2023-02-03 07:57:41 +0100

    Basic html layout
    Basic html structure build

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", indexFunc)
+	r.Get("/switzerland", swissFunc)
+	r.Get("/germany", germanFunc)
+	r.Get("/france", frenchFunc)
 
 	r.Handle("/style/*", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
 	http.ListenAndServe(":8080", r)
@@ -24,6 +27,39 @@ func indexFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err = t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func swissFunc(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("").ParseFiles("view\\switzerland.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = t.ExecuteTemplate(w, "switzerland.html", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func germanFunc(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("").ParseFiles("view\\germany.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = t.ExecuteTemplate(w, "germany.html", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func frenchFunc(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("").ParseFiles("view\\france.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = t.ExecuteTemplate(w, "france.html", nil); err != nil {
 		log.Fatal(err)
 	}
 }
